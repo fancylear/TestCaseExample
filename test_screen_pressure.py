6d626e373ee4de71cea918cf9719747e13830e8b
fancylear <fancylear@qq.com>
2014-09-24 10:27:15 +0800

    屏幕压力测试
    检测

--- test_screen_pressure.py
+++ test_screen_pressure.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 from uiautomator import device as d
+
 import time
 
-# 开关屏幕
+# 开关屏幕50次
 for i in range(1,50):
-    d.screen.on()
-    time.sleep(0.5)
-    d.screen.off()
+    d.sleep()
+    time.sleep(0.2)
+    d.wakeup()
+    time.sleep(0.2)
+    # 新增wrapper，检查屏幕是否开启
+    # 若没有开启则不正常
+    if not d.screen.ison()
+        print("第" + i + "次没有正常开启屏幕")
